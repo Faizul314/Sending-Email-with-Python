a93fbb755a5bcd928fb5fc9ec97c47abf733c565
Faizul Islam <faizulfzs61@gmail.com>
2020-07-31 14:07:57 +0600

    updated

--- email_sender.py
+++ email_sender.py
@@ -6,14 +6,14 @@ from pathlib import Path
 content = Path('./index.html')
 c = Template(content.read_text())
 message = EmailMessage()
-message['from'] = 'No name'
-message['to'] = '<email id>'
+message['from'] = '<sender_name>'
+message['to'] = '<receiver_email_id>'
 message['subject'] = 'Greetings'
-message.set_content(c.substitute(name = '<name>'), 'html')
+message.set_content(c.substitute(name = '<receiver_name>'), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as s:
     s.ehlo()
     s.starttls()
-    s.login('<email id>', '<password>')
+    s.login('<sender_email_id>', '<sender_email_pass>')
     s.send_message(message)
     print('done')
